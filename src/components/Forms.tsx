a85067cb38f2a4906c156123fb3ca3c5a4a3a59f
Lucas Henrique Torresin da Costa <lucas_hcosta@live.com>
2023-07-10 15:14:00 -0300

    Fetch, form nav

--- src/components/Forms.tsx
+++ src/components/Forms.tsx
@@ -4,25 +4,42 @@ import { PiMagnifyingGlass } from 'react-icons/pi'
 import getData from "@/utils/api"
 import { useState } from "react"
 import Card from "./Card"
+import { useRouter } from "next/navigation"
 
 export default function Forms() {
   const [form, setForm] = useState('')
-  const [location, setLocation] = useState<any>([]);
-  const [current, setCurrent] = useState<any>([])
-  const [forecast, setForecast] = useState<any>([])
+  
+  const router = useRouter()
 
-  const dataFunction = async (parametro: string) => {
-    const data: dataProps = await getData(parametro)
-    setLocation(data.location)
-    setCurrent(data.current)
-    setForecast(data.forecast)
-    return data;
+  const handleClick = (e: React.FormEvent<HTMLFormElement>)=>{
+    e.preventDefault();
+
+    if(form === ''){
+      return alert('Please fill in the search bar')
+    };
+
+    updateSearchParams(form.toLowerCase())
+  }
+  
+  const updateSearchParams = (form:string,)=>{
+    const searchParams = new URLSearchParams(window.location.search);
+
+    if(form){
+      searchParams.set('city', form)
+    } else{
+      searchParams.delete('city')
+    }
+
+
+    const newPathname=`${window.location.pathname}?${searchParams.toString()}`
+
+    router.push(newPathname)
   }
 
-  const handleClick = (e: { preventDefault: () => void }) => {
-    e.preventDefault()
-    dataFunction(form)
-  }
+
+  
+
+
 
   return (
     <>
@@ -30,9 +47,6 @@ export default function Forms() {
         <input className="bg-transparent border-white border rounded-lg h-7 text-white" value={form} onChange={(e) => setForm(e.target.value)} type="text" />
         <button onClick={handleClick}><PiMagnifyingGlass className="text-white" size={20} /></button>
       </form>
-      <div>
-        {/* <Card location={location} current={current} forecast={forecast} /> */}
-      </div>
     </>
   )
 }
